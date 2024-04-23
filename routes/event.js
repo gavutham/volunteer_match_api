b23f82defea15cc44ed69f81501eda770f9b26d8
Gavutham K <kgavutham2005@gmail.com>
2024-04-23 14:47:34 +0530

    create function to get all event

--- routes/event.js
+++ routes/event.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const Event = require("../models/Event");
 
+router.get("/", async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.status(200).json(events);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const newEvent = new Event(req.body);
